82d8ee77707ac395ad41b0e0993fea3844a39dee
Godrik <krisapios@gmail.com>
2022-09-30 19:42:15 +0500

    update

--- src/Main.java
+++ src/Main.java
@@ -11,19 +11,21 @@ public class Main {
 
             int tickets = person.getTicket();
             if (tickets > 0) {
+                System.out.println(person.surname + " " + person.name + " имеет " + tickets + " билетов");
                 person.setTicket(--tickets);
                 clients.offer(person);
                 System.out.println(person.toString());
             }
         }
         System.out.println("Билеты кончились");
+        System.out.println(clients.isEmpty());
     }
 
     static List<Person> generateClients() {
         return List.of(
                 new Person("Иван", "Петров", 2),
                 new Person("Евгений", "Букин", 4),
-                new Person("Петр", "Жириновский", 1),
+                new Person("Петр", "Жириновский", 0),
                 new Person("Артем", "Kи", 9),
                 new Person("Данил", "Кораблев", 5)
         );
